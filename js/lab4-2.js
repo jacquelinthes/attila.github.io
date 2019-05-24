d40093f69dbce2e45209b8f183d2d169f6870e52
jacquelinthes <the16001@byui.edu>
2019-05-24 15:34:44 -0400

    lab2

---
+++ js/lab4-2.js
@@ -0,0 +1,10 @@
+function applyScripture(){
+ 	// INPUT
+    let name = document.getElementById('nameInputBox').value;
+    //PROCESSING
+    let scripture = "And again I, " + name + ", would exhort you that ye would come unto Christ," +
+        " and lay hold upon every good gift, and touch not evil gift," +
+        " nor the unclean thing."
+    // OUTPUT
+    document.getElementById("output").innerHTML = scripture;
+}
