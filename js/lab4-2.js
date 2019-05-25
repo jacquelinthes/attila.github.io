7bc1771ad10d83260a6e5e36bd6f786b636df98b
jacquelinthes <the16001@byui.edu>
2019-05-24 20:25:17 -0400

    vc

--- js/lab4-2.js
+++ js/lab4-2.js
@@ -2,9 +2,11 @@ function applyscripture(){
  	// INPUT
     let name = document.getElementById('nameInputBox').value;
     //PROCESSING
-    let scripture = "And again I, " + name + ", would exhort you that ye would come unto Christ," +
-        " and lay hold upon every good gift, and touch not evil gift," +
-        " nor the unclean thing."
+    let scripture = "I, " + name + ", having been born of goodly parents," +
+        " therefore I was taught somewhat in all the learning of my father; and having seen many afflictions in the course of my days, nevertheless," +
+        " having been highly favored of the Lord in all my days;" + "yea, having had a great knowledge of the goodness and the mysteries of God, therefore I make a record of my proceedings in my days. "
     // OUTPUT
     document.getElementById("output").innerHTML = scripture;
 }
+
+
