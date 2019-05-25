b0dd4f36aa959f50493cb20b1c509f2c9c50940a
jacquelinthes <the16001@byui.edu>
2019-05-24 20:53:36 -0400

    sum

---
+++ js/lab4-3.js
@@ -0,0 +1,10 @@
+function addressbuilder(){
+	// INPUT
+	let x = document.getElementById('city').value;
+    let y = document.getElementById('state').value;
+    let z = parseFloat(document.getElementById('zipcode').value);
+    // PROCESSING
+	let sum = x + y + z;
+    // OUTPUT
+	document.getElementById("output").innerHTML = sum;
+}
