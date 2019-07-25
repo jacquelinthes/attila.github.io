de8e72f6c82fff926c837dca85dbfa254de46981
jacquelinthes <the16001@byui.edu>
2019-07-25 09:03:55 -0400

    pog

---
+++ js/lab12-2.js
@@ -0,0 +1,23 @@
+Get Current Date
+let today = new Date(); // new Date object
+// now concatenate formatted output
+let date = (today.getMonth()+1) + " / " +  today.getDate() + " / " + today.getFullYear();
+document.getElementById('currentdate').innerHTML = date;
+
+//*Input: Create one or more arrays manually or dynamically.
+Processing: Returns the value of the middle element in an array. If the array has an even number of elements, then this function must return the average of the two middle elements.
+Output: The value of the middle element  
+*/
+
+function getMiddle(list) {
+  list= [69,30,1,100,35,29];
+  let middleNumber= 0;
+  numLength =list.length;
+  if(numLength%2===0){
+    middleNumber=(list[numLength/2-1]+list[numLength/2])/2;
+  } else{ 
+  answer =list[(numLength-1)/2];
+  }
+  document.getElementById("arrayA").innerHTML= middleNumber; 
+  return middleNumber; 
+}
